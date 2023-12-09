2e507e04c40be662ce6cf3d6814710893c3847e8
ndqkhanh20 <khanhndq2002@gmail.com>
2023-12-09 22:09:09 +0700

    fix: fix layout of ads spot list

--- admin/src/views/admin/adsSpots/AdsSpotList.js
+++ admin/src/views/admin/adsSpots/AdsSpotList.js
@@ -1,26 +1,25 @@
 import React, { useEffect, useState } from 'react'
 
-import { Box, Button, Grid, Checkbox } from '@mui/material'
+import { Box, Checkbox } from '@mui/material'
 import { DataGrid, gridClasses } from '@mui/x-data-grid'
 import { CCard, CCardBody } from '@coreui/react'
 import { grey } from '@mui/material/colors'
 import { GRID_DEFAULT_LOCALE_TEXT } from 'src/components/CustomGridLocale'
-import { cilPen, cilTrash } from '@coreui/icons'
-import CIcon from '@coreui/icons-react'
 import CustomNoRowsOverlay from 'src/components/CustomNoRowsOverlay'
 import CustomGridToolbar from 'src/components/CustomGridToolbar'
+import { useNavigate } from 'react-router-dom'
 
 const columns = [
-  { field: 'id', headerName: 'STT', width: 50 },
+  { field: 'id', headerName: 'STT', width: 70 },
   {
     field: 'address',
     headerName: 'Địa điểm',
-    width: 340,
+    width: 400,
   },
   {
     field: 'spot_type_name',
     headerName: 'Loại vị trí',
-    width: 340,
+    width: 400,
     renderCell: (cellValues) => {
       const spotType = cellValues.row['spot_type']
       return <span>{spotType.name}</span>
@@ -38,7 +37,8 @@ const columns = [
   {
     field: 'is_available',
     headerName: 'Đã quy hoạch',
-    width: 150,
+    // width: 150,
+    flex: 1,
     type: 'boolean',
     sortable: false,
     renderCell: (cellValues) => {
@@ -50,41 +50,41 @@ const columns = [
       )
     },
   },
-  {
-    field: 'actions',
-    headerName: 'Thao tác',
-    type: 'actions',
-    flex: 1,
-    sortable: false,
-    renderCell: (params) => (
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <Button
-            variant="contained"
-            color="primary"
-            size="small"
-            flex={1}
-            startIcon={<CIcon icon={cilPen} />}
-            onClick={() => console.log(params.row.id)}
-          >
-            Sửa
-          </Button>
-        </Grid>
-        <Grid item xs={12} md={6}>
-          <Button
-            variant="contained"
-            color="error"
-            size="small"
-            flex={1}
-            startIcon={<CIcon icon={cilTrash} />}
-            onClick={() => console.log(params.row.id)}
-          >
-            Xóa
-          </Button>
-        </Grid>
-      </Grid>
-    ),
-  },
+  // {
+  //   field: 'actions',
+  //   headerName: 'Thao tác',
+  //   type: 'actions',
+  //   flex: 1,
+  //   sortable: false,
+  //   renderCell: (params) => (
+  //     <Grid container spacing={2}>
+  //       <Grid item xs={12} md={6}>
+  //         <Button
+  //           variant="contained"
+  //           color="primary"
+  //           size="small"
+  //           flex={1}
+  //           startIcon={<CIcon icon={cilPen} />}
+  //           onClick={() => console.log(params.row.id)}
+  //         >
+  //           Sửa
+  //         </Button>
+  //       </Grid>
+  //       <Grid item xs={12} md={6}>
+  //         <Button
+  //           variant="contained"
+  //           color="error"
+  //           size="small"
+  //           flex={1}
+  //           startIcon={<CIcon icon={cilTrash} />}
+  //           onClick={() => console.log(params.row.id)}
+  //         >
+  //           Xóa
+  //         </Button>
+  //       </Grid>
+  //     </Grid>
+  //   ),
+  // },
 ]
 
 const AdsSpotList = () => {
@@ -95,6 +95,8 @@ const AdsSpotList = () => {
     pageSize: 25,
     page: 0,
   })
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     const fetchData = async () => {
@@ -152,7 +154,8 @@ const AdsSpotList = () => {
             rowSelection={false}
             onRowClick={(params) => {
               // TODO: handle row click
-              console.log(params.row.id)
+              // console.log(params.row.id)
+              navigate(`/admin/ads_spots/${params.row.id}`)
             }}
             paginationModel={{ page: data.page, pageSize: data.pageSize }}
             onPaginationModelChange={(params) => {
