efda3c4903d2b39018d0a22a89f9f423d6148886
ndqkhanh20 <khanhndq2002@gmail.com>
2023-12-15 16:10:17 +0700

    feat: add missing validations to adsPanel and adsSpot

--- backend/src/validations/adsPanel.validation.js
+++ backend/src/validations/adsPanel.validation.js
@@ -6,11 +6,28 @@ const getAdsPanel = {
   }),
 };
 
-const createAdsPanel = {};
+const createAdsPanel = {
+  body: Joi.object().keys({
+    type_id: Joi.number().required(),
+    height: Joi.number().required(),
+    width: Joi.number().required(),
+    expire_date: Joi.date().timestamp().required(),
+    image: Joi.string().required(),
+    ads_spot_id: Joi.number().required(),
+  }),
+};
 
 const updateAdsPanel = {
   params: Joi.object().keys({
     id: Joi.number().required(),
+  }),
+  body: Joi.object().keys({
+    type_id: Joi.number().required(),
+    height: Joi.number().required(),
+    width: Joi.number().required(),
+    expire_date: Joi.date().timestamp().required(),
+    image: Joi.string().required(),
+    ads_spot_id: Joi.number().required(),
   }),
 };
 
--- backend/src/validations/adsSpot.validation.js
+++ backend/src/validations/adsSpot.validation.js
@@ -6,11 +6,36 @@ const getAdsSpot = {
   }),
 };
 
-const createAdsSpot = {};
+const createAdsSpot = {
+  body: Joi.object().keys({
+    address: Joi.string().required(),
+    ward_id: Joi.number().required(),
+    district_id: Joi.number().required(),
+    spot_type_id: Joi.number().required(),
+    ads_type_id: Joi.number().required(),
+    image: Joi.string().required(),
+    is_available: Joi.boolean().required(),
+    max_ads_panels: Joi.number().required(),
+    latitude: Joi.number().required(),
+    longtitude: Joi.number().required(),
+  }),
+};
 
 const updateAdsSpot = {
   params: Joi.object().keys({
     id: Joi.number().required(),
+  }),
+  body: Joi.object().keys({
+    address: Joi.string().required(),
+    ward_id: Joi.number().required(),
+    district_id: Joi.number().required(),
+    spot_type_id: Joi.number().required(),
+    ads_type_id: Joi.number().required(),
+    image: Joi.string().required(),
+    is_available: Joi.boolean().required(),
+    max_ads_panels: Joi.number().required(),
+    latitude: Joi.number().required(),
+    longtitude: Joi.number().required(),
   }),
 };
 
