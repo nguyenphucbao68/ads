5e81830fba60dd3a2b801e78f26191970d389179
krnthoang542 <81230977+simplord542@users.noreply.github.com>
2024-01-01 18:45:19 +0700

    report validation

--- backend/src/validations/report.validation.js
+++ backend/src/validations/report.validation.js
@@ -6,7 +6,23 @@ const getReports = {
     did: Joi.number(),
   }),
 };
-
+const createReports = {
+  body: Joi.object().keys({
+    report_type_id: Joi.number().integer().required(),
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+    content: Joi.string().required(),
+    ads_panel_id: Joi.number().integer(),
+    address: Joi.string(),
+    ward_id: Joi.number().integer(),
+    district_id: Joi.number().integer(),
+  }).or('ads_panel_id', 'ward_id')
+  .with('ward_id', ['district_id', 'address'])
+  .with('district_id', ['ward_id', 'address'])
+  .with('address', ['district_id', 'ward_id'])
+}
 module.exports = {
   getReports,
+  createReports
 };
