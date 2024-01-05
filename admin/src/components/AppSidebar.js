baf2eef0c1640c005003f520e498d0aed5eef5b1
ndqkhanh20 <khanhndq2002@gmail.com>
2024-01-05 23:03:52 +0700

    refactor: change app icon & title and fix sidebar

--- admin/src/components/AppSidebar.js
+++ admin/src/components/AppSidebar.js
@@ -2,19 +2,14 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { CSidebar, CSidebarBrand, CSidebarNav, CSidebarToggler } from '@coreui/react'
-import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
-
-import { logoNegative } from 'src/assets/brand/logo-negative'
-import { sygnet } from 'src/assets/brand/sygnet'
 
 import SimpleBar from 'simplebar-react'
 import 'simplebar/dist/simplebar.min.css'
 
 // sidebar nav config
 import navigation from '../_nav'
-import { Box } from '@mui/material'
 import { ReactComponent as AdministratorIcon } from 'src/assets/icons/administrator.svg'
 
 const AppSidebar = () => {
@@ -34,19 +29,6 @@ const AppSidebar = () => {
       }}
     >
       <CSidebarBrand className="d-none d-md-flex" to="/">
-        <CIcon className="sidebar-brand-full" icon={logoNegative} height={35} />
-        <CIcon className="sidebar-brand-narrow" icon={sygnet} height={35} />
-      </CSidebarBrand>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'start',
-          alignItems: 'center',
-          padding: '10px 0',
-          paddingLeft: '20px',
-        }}
-      >
         <AdministratorIcon
           style={{
             color: '#ffffff',
@@ -62,7 +44,7 @@ const AppSidebar = () => {
             ? `Cán bộ ${user?.district?.name || 'Quận'}`
             : `Cán bộ ${user?.ward?.name || 'Phường'}`}
         </span>
-      </Box>
+      </CSidebarBrand>
       <CSidebarNav>
         <SimpleBar>
           <AppSidebarNav items={navigation} />
